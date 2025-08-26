1a73e93882d78fac0285e19398ea67a62419e1fe
Githubshakil <shakilmahmud007@gmail.com>
2025-08-26 23:33:26 +0600

    update
    routeing_expresds

--- routeing_express/index.js
+++ routeing_express/index.js
@@ -1,11 +1,28 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// middleware to parse JSON request bodies
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("well come to my server 2.0");
+});
+
+// get request
+
+app.get("/about", (req, res) => {
+  res.send("This is a About Page");
+});
+
+// post request
+
+app.post("/contact", (req, res) => {
+  // console.log("posting contact details");
+  console.log(req.body);
+
+  res.send("massage recived");
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
