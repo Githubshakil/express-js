43665ab9b62b459b4d36e0bcc8a55b8735889465
Githubshakil <shakilmahmud007@gmail.com>
2025-09-05 19:59:30 +0600

    update
    blogs data

--- basic-blogs-api/src/routes/blogRoutes.js
+++ basic-blogs-api/src/routes/blogRoutes.js
@@ -2,9 +2,65 @@
 
  const router = express.Router()
 
- router.get('/', (req, res)=>{
-    res.send('Hi,  this is from blog routes')
- })
+let blogs = [
+    
+  {
+    "id": 1,
+    "title": "First Blog Post",
+    "description": "This is the first blog post description"
+  },
+  {
+    "id": 2,
+    "title": "Getting Started with Web Development",
+    "description": "An introduction to the basics of web development for beginners."
+  },
+  {
+    "id": 3,
+    "title": "Understanding JavaScript",
+    "description": "A beginner-friendly guide to learning JavaScript fundamentals."
+  },
+  {
+    "id": 4,
+    "title": "CSS Tips and Tricks",
+    "description": "Enhance your web pages with these useful CSS styling techniques."
+  },
+  {
+    "id": 5,
+    "title": "Mastering React Components",
+    "description": "Learn how to create and reuse components in React applications."
+  },
+  {
+    "id": 6,
+    "title": "The Importance of Responsive Design",
+    "description": "Why responsive design matters and how to implement it."
+  },
+  {
+    "id": 7,
+    "title": "Boosting Website Performance",
+    "description": "Practical tips to make your website faster and more efficient."
+  },
+  {
+    "id": 8,
+    "title": "Exploring Node.js",
+    "description": "An overview of Node.js and how it powers modern web applications."
+  },
+  {
+    "id": 9,
+    "title": "Introduction to APIs",
+    "description": "What APIs are and how developers use them to connect systems."
+  },
+  {
+    "id": 10,
+    "title": "Future of Web Development",
+    "description": "A look at upcoming trends and technologies shaping the web."
+  }
+]
+
+//get all blogs
+router.get('/',(req,res)=>{
+    res.send(blogs)
+})
+
 
 
 
