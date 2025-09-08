9542feb61bcbc5325cf25e24bb9bb41df5dbf09f
Githubshakil <shakilmahmud007@gmail.com>
2025-09-08 19:35:22 +0600

    update
    Request (req) Object in Details

---
+++ handle-request-response/index.js
@@ -0,0 +1,35 @@
+const express = require("express")
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get("/", (req,res)=>{
+    res.send("server is running!")
+})
+
+app.get("/inspect", (req,res)=>{
+    console.log("Request method:",req.method)
+    console.log("Request url:",req.url)
+    // console.log("paraMetters:", req.params)
+    console.log("query:",req.query)
+    console.log("headers:",req.headers)
+    res.send("inspect Request now")
+})
+
+//res object in details
+
+app.get("/text", (req,res)=>{
+    res.send("this is  a text route")
+})
+app.get("/json-data", (req,res)=>{
+    res.json({massage: "hello this is a json data"})
+})
+app.get("/status", (req,res)=>{
+    res.status(404).send("page not found")
+    // res.status(200).send("ok")
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`)
+})
