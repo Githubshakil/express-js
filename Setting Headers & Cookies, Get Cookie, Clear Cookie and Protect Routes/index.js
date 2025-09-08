97ccbbcffd7822ba2e7be1fba2ea145b65933a42
Githubshakil <shakilmahmud007@gmail.com>
2025-09-08 20:59:04 +0600

    update

--- Setting Headers & Cookies, Get Cookie, Clear Cookie and Protect Routes/index.js
+++ Setting Headers & Cookies, Get Cookie, Clear Cookie and Protect Routes/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("server is running!");
+  res.send("server is running...");
 });
 
 //set headers
