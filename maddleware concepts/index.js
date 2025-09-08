3e703b5d0863fb94471068f846544cde5774a42b
Githubshakil <shakilmahmud007@gmail.com>
2025-09-08 12:57:34 +0600

    update
    Built-in Middleware (express.json(), express.urlencoded() and express.static())

--- maddleware concepts/index.js
+++ maddleware concepts/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const activityLogger = require('./src/middleware/logger')
+const myMiddleware = require('./src/middleware/myMiddleware')
 const app = express()
 const port = process.env.PORT || 5050
+
+//defult middleware
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static('src/public'))
 
 //use the custom middleware globally
 app.use(activityLogger)
@@ -10,15 +17,17 @@ app.get('/', (req, res) => {
   res.send('Well come to our server!')
 })
 
+//post new blog routes
+app.post('/new-blog',(req,res)=>{
+  console.log(req.body)
+  res.send('Blog create successfully!')
+})
+
 app.get("/about", (req,res)=>{
   res.send("About Page")
 })
 
-//middleware structure
-const myMiddleware = (req,res,next) => {
-    console.log("Middleware excuated!")
-    next()
-} 
+
 app.get('/products',myMiddleware, (req,res) =>{
     res.send("product page")
 })
