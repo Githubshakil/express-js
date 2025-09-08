83f6cbafa5c35da165ea2d7934d7d2ff264231b4
Githubshakil <shakilmahmud007@gmail.com>
2025-09-08 12:02:35 +0600

    update
    middleware

--- maddleware concepts/index.js
+++ maddleware concepts/index.js
@@ -3,7 +3,16 @@ const app = express()
 const port = process.env.PORT || 5050
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Well come to our server!')
+})
+
+//middleware structure
+const myMiddleware = (req,res,next) => {
+    console.log("Middleware excuated!")
+    next()
+} 
+app.get('/products',myMiddleware, (req,res) =>{
+    res.send("product page")
 })
 
 app.listen(port, () => {
