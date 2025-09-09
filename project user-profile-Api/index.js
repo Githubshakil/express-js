e51a005aee9806f437b7ffce0c6aadc912035e2d
Githubshakil <shakilmahmud007@gmail.com>
2025-09-09 19:15:43 +0600

    update
    User Profile API Project

--- project user-profile-Api/index.js
+++ project user-profile-Api/index.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const cookieParser = require('cookie-parser')
 const port = process.env.PORT || 3000
+
+
+//middleware
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('Project User Profile API Running....')
@@ -35,14 +50,42 @@ app.get("/api/profile", (req, res)=>{
 
 //set cookies
 app.get('/set-custom-cookies', (req, res) =>{
-    res.cookie('costum-cookie', '123abc', {
+    res.cookie('customCookie', 'shakil1234567890', {
         maxAge: 60000,
         httpOnly:true,
-        secure:true,
+        secure:false,
 
     })
     res.send(`cookie set successfully`)
 })
+
+//get cookies
+
+app.get("/get-custom-cookies", (req,res)=>{
+    const cookie = req.cookies.customCookie
+    console.log(cookie)
+    res.send(`your custom cookie is : ${cookie}`)
+})
+
+//Delete cookies
+app.get('/delete-custom-cookies', (req,res)=>{
+    res.clearCookie('customCookie')
+    res.send('cookie delete successfully')
+})
+
+//profile html page
+
+app.get('/profile', (req,res)=>{
+    const cookie = req.cookies.customCookie
+    if (!cookie) {
+        return res.status(401).json({massage: "UnAuthorized access"})
+    }
+    const filePath = path.join(process.cwd(),'public', 'profile.html')
+    res.sendFile(filePath)
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
