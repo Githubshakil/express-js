0d542a2363aa2984d49ca32e547e4f4248dc885e
Githubshakil <shakilmahmud007@gmail.com>
2025-09-14 20:31:11 +0600

    update

--- logging errors debugging/middleware/errorHandler.js
+++ logging errors debugging/middleware/errorHandler.js
@@ -1,5 +1,9 @@
 const logger = require("./logger")
 
 const errorHandler = (err, req, res, next) => {
-    logger.error(`${err.st}`)
+    logger.error(`${err.statusCode || 500} - ${err.message} - ${req.orginalUrl}`)
+    res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    })
+
 }
