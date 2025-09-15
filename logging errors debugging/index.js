d97ef8dac515d381549753f9300c3977b3c57eca
Githubshakil <shakilmahmud007@gmail.com>
2025-09-15 19:23:11 +0600

    update

--- logging errors debugging/index.js
+++ logging errors debugging/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const errorHandler = require('./middleware/errorHandler')
 const app = express()
 const port = process.env.PORT || 3000
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res, next) => {
+  const error = new Error("Not found home page")
+  next(error)
 })
+
+//call middleware
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- logging errors debugging/middleware/errorHandler.js
+++ logging errors debugging/middleware/errorHandler.js
@@ -1,9 +1,13 @@
 const logger = require("./logger")
 
+
+
 const errorHandler = (err, req, res, next) => {
     logger.error(`${err.statusCode || 500} - ${err.message} - ${req.orginalUrl}`)
     res.status(err.statusCode || 500).json({
+        success: false,
         message: err.message || "Internal Server Error"
     })
 
 }
+module.exports = errorHandler
