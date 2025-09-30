3d26e18da7fd611a27e8dade0ec818ad25a4b348
Githubshakil <shakilmahmud007@gmail.com>
2025-09-30 23:50:10 +0600

    update

--- upload image by multer/index.js
+++ upload image by multer/index.js
@@ -1,42 +1,36 @@
-const { error } = require('console')
-const express = require('express')
-const app = express()
-const multer  = require('multer')
-const path = require("path")
-const port = 3000
-
+const { error } = require("console");
+const express = require("express");
+const app = express();
+const multer = require("multer");
+const path = require("path");
+const port = 3000;
 
 //multer file storage
 const storage = multer.diskStorage({
-    destination:"uploads/",
-    filename:(req, file, cb)=>{
-        cb(null, Date.now() + path.extname(file.originalname))  //unique file name
-    }
-})
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); //unique file name
+  },
+});
 
-const upload = multer({ storage })
-
+const upload = multer({ storage });
 
 //middleware
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use("/uploads", express.static("uploads"));
 
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/public/index.html");
+});
 
-
-
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-app.post("/upload",upload.single('image') ,(req, res)=>{
-if (!req.file) return res.status(400).json({error: 'file not found'}) 
-    res.status(200).json({
-        message: "File Uploaded sucessfully",
-        file: req.file.filename
-})
-    
-
-})
+app.post("/upload", upload.single("image"), (req, res) => {
+  if (!req.file) return res.status(400).json({ error: "file not found" });
+  res.status(200).json({
+    message: "File Uploaded sucessfully",
+    file: req.file.filename,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
