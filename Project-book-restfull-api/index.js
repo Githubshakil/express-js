6bdaab8764e73f23ec7d20c40298b648fddee380
Githubshakil <shakilmahmud007@gmail.com>
2025-10-08 23:25:15 +0600

    update
    CORS

--- Project-book-restfull-api/index.js
+++ Project-book-restfull-api/index.js
@@ -1,25 +1,26 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 
-
-const port =process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 //middleware
-app.use(express.json())
-app.use(cors())
-
+app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 //routes
-const bookRoutes = require('./src/routes/bookRoutes.js')
-app.use('/books', bookRoutes)
+const bookRoutes = require("./src/routes/bookRoutes.js");
+app.use("/books", bookRoutes);
 
-
-app.get('/', (req, res) => {
-  res.send('Wellcome to the Book Library API')
-})
+app.get("/", (req, res) => {
+  res.send("Wellcome to the Book Library API");
+});
 
 app.listen(port, () => {
-  console.log(`Wellcome to the Book Library API listening on port ${port}`)
-})
+  console.log(`Wellcome to the Book Library API listening on port ${port}`);
+});
