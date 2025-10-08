1f13eabbb1cfcf9b1b198faaad75570456fcdb60
Githubshakil <shakilmahmud007@gmail.com>
2025-10-09 00:51:32 +0600

    update
    caching strategies node-cache

--- performance-optimizition/index.js
+++ performance-optimizition/index.js
@@ -2,10 +2,27 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const winston = require('winston')
+const NodeCache = require( "node-cache" );
 
 const port = process.env.PORT || 3000
 
+
+const cache = new NodeCache({stdTTL: 100, checkperiod: 120 });
+
 //middleware
+
+const cacheMiddleware = (req,res,next) =>{
+    const key = req.originalUrl
+const cacheData = cache.get(key)
+
+if (cacheData) {
+    console.log("Data cached successfully")
+    return res.json(cacheData)
+}
+
+console.log("First time request, so no caches")
+next()
+}
 
 app.use(morgan("dev"))
 
@@ -24,6 +41,21 @@ const logger = winston.createLogger({
 app.get('/', (req, res) => {
     logger.info("From root route")
   res.send('Performance Optimizition Module')
+})
+
+// caching a route
+
+app.get("/data",cacheMiddleware ,(req,res)=>{
+    const userData= {
+        name: "shakil",
+        age: 40,
+        email:"shakilmahmud007@gmail.com"
+    }
+
+    //set the cache to store to temp memory
+
+    cache.set(req.originalUrl, userData)
+    res.json(userData)
 })
 
 app.listen(port, () => {
