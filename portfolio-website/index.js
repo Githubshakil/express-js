3ec05886988c0a893b1ba3c045011f07579fd5c8
Githubshakil <shakilmahmud007@gmail.com>
2025-10-09 13:31:00 +0600

    update

--- portfolio-website/index.js
+++ portfolio-website/index.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const port =process.env.PORT || 3000
+const express = require("express");
+const app = express();
+const cors = require('cors')
+const ejs = require('ejs')
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json())
+app.use(cors())
+app.use(express.static("public"))
+app.set('view engine', 'ejs')
 
 
-
-app.get('/', (req, res) => {
-  res.send('portfolio Website')
-})
+//routes
+app.get("/", (req, res) => {res.render("index")});
+app.get("/about", (req, res) => {res.render("about")});
+app.get("/project", (req, res) => {res.render("project")});
+app.get("/contact", (req, res) => {res.render("contact")});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
